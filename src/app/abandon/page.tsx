8c5abdaa381a29f11b2959097b3ce52f17e9632b
g2 Hong <hon9g2@gmail.com>
2024-03-09 18:36:11 +0900

    chore: update layout

---
+++ src/app/abandon/page.tsx
@@ -0,0 +1,3 @@
+export default function Abandon() {
+  return <h1>Hello, Abandon!</h1>;
+}
---
+++ src/app/lost/page.tsx
@@ -0,0 +1,5 @@
+import Image from "next/image";
+
+export default function Lost() {
+  return <h1>Hello, Lost!</h1>;
+}

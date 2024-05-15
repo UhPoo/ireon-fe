5491a4a95aec28b4de59a8f1ae5877a25c9a0e28
g2 Hong <hon9g2@gmail.com>
2024-05-15 21:11:21 +0900

    chore: button ghost style 추가

--- src/app/_components/Button.tsx
+++ src/app/_components/Button.tsx
@@ -1,24 +1,24 @@
 import { ButtonHTMLAttributes } from "react";
 import { twMerge } from "tailwind-merge";
 
-interface IButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  variant?: "primary_filled" | "primary_outlined" | "red_filled";
-}
+const VARIANTS = {
+  primary_filled: "bg-green_3BAE66 hover:bg-green_209957 text-white",
+  primary_outlined:
+    "bg-white border-[1px] border-green_3BAE66 text-green_3BAE66 hover:border-green_209957 hover:text-green_209957",
+  red_filled: "bg-red_EF4444 hover:bg-red_DC2626 text-white",
+  ghost: "bg-transparent text-green_3BAE66 hover:bg-opacity-50",
+};
+
+type IButtonProps = {
+  variant?: "primary_filled" | "primary_outlined" | "red_filled" | "ghost";
+} & ButtonHTMLAttributes<HTMLButtonElement>;
 
 export default function Button({
   variant = "primary_filled",
   children,
   className,
   onClick,
-  ...props
 }: IButtonProps) {
-  const VARIANTS = {
-    primary_filled: "bg-green_3BAE66 hover:bg-green_209957 text-white",
-    primary_outlined:
-      "bg-white border-[1px] border-green_3BAE66 text-green_3BAE66 hover:border-green_209957 hover:text-green_209957",
-    red_filled: "bg-red_EF4444 hover:bg-red_DC2626 text-white",
-  };
-
   return (
     <button
       className={twMerge(
@@ -28,8 +28,7 @@ export default function Button({
         variant && VARIANTS[variant],
         className && className
       )}
-      onClick={onClick}
-    >
+      onClick={onClick}>
       {children && children}
     </button>
   );

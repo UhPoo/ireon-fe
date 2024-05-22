2302f903d4343812261e8fd447748d735c228e2e
zeon8080 <jec0228@naver.com>
2024-05-22 22:14:34 +0900

    design: 탭기능 구현

--- src/app/(route)/lost/page.tsx
+++ src/app/(route)/lost/page.tsx
@@ -1,14 +1,42 @@
 "use client";
 // Lost.tsx
+import { useState } from "react";
 import Map from "./maps";
+import LostList from "./list";
 
-const Lost = () => {
+export default function Lost() {
+  const components: Record<number, JSX.Element> = {
+    0: <Map />,
+    1: <LostList />,
+  };
+
+  const [selTab, setSelTab] = useState(0);
+
+  const clickHandler = (idx: number) => {
+    setSelTab(idx);
+  };
+
   return (
     <div>
-      <h1>Hello, Lost!</h1>
-      <Map />
+      <div className=" h-[50px]">
+        <div className="flex h-full items-end">
+          <button
+            type="button"
+            className={`w-[486px] border-gray-300 rounded-t-[12px] mr-[-1px] border ${selTab === 0 ? " h-[46px] text-green-500" : "bg-gray-50 h-[36px]"}`}
+            onClick={() => clickHandler(0)}
+          >
+            맵
+          </button>
+          <button
+            type="button"
+            className={`w-[486px] border-gray-300 rounded-t-[12px] border ${selTab === 1 ? " h-[46px] text-green-500" : "bg-gray-50 h-[36px]"}`}
+            onClick={() => clickHandler(1)}
+          >
+            리스트
+          </button>
+        </div>
+      </div>
+      <div>{components[selTab]}</div>
     </div>
   );
-};
-
-export default Lost;
+}
